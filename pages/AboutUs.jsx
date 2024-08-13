c4b4c66b564ea1bf61f901141294d8a8cd055024
kd4-aishawarya-83543 <aishwaryalaturkar47@gmail.com>
2024-08-13 11:33:40 +0530

    frontend_Pages_uploded

---
+++ pages/AboutUs.jsx
@@ -0,0 +1,21 @@
+
+
+const AboutUs = () => {
+  return (
+    <div className="text-color ms-5 me-5 mr-5 mt-3">
+      <b>
+      Mobile app stores are digital marketplaces that allow app owners to list and sell their mobile applications and consumers to discover and buy them.
+        <br />
+        <br />
+        The biggest app stores tend to list apps native to particular operating systems (OS) like iOS and Android, whereas multi-platform app stores list several.
+        <br />
+        <br />
+
+        A mobile application or app is a computer program or software application designed to run on a mobile device such as a phone, tablet, or watch.
+        
+      </b>
+    </div>
+  );
+};
+
+export default AboutUs;
---
+++ pages/ContactUs.jsx
@@ -0,0 +1,16 @@
+
+const ContactUs = () => {
+  return (
+    <div className="text-color ms-5 me-5 mr-5 mt-3">
+      <b>
+      Have a question or query you need answered? Contact HERE directly for personalized assistance and further information about the power of location.
+        
+        <br />
+        <br />
+        The preferred alternatives are “please don’t hesitate to reach out,” “I’m here if you have any further questions,” and “if you need further clarification, let me know.” These options present a slightly more formal choice while remaining polite to the person who needs help.
+      </b>
+    </div>
+  );
+};
+
+export default ContactUs;
